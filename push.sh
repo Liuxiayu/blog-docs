00eeb723b765727c1ef38c246b5e58249faffa69
root <root@localhost.localdomain>
2022-04-07 18:22:36 +0800

    test

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf /tmp/myblog_container/*
-cp -rp /root/test/blog-docs/* /tmp/myblog_container/
+cp -rp /root/blog-docs/* /tmp/myblog_container/
 git add --all
 git commit -m "test"
 git push -u origin master
